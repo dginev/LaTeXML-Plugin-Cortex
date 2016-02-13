4b73d94c13556decfd7f15d4242851ed2a99a33f
Deyan Ginev <d.ginev@jacobs-university.de>
2016-02-13 16:29:49 -0500

    uninstall LaTeXML before reinstall

--- scripts/update_kwarc_all.pl
+++ scripts/update_kwarc_all.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl -w
 
-# We assume script user has their public keys added to all relevant machines' ~/.ssh/authorized_keys 
+# We assume script user has their public keys added to all relevant machines' ~/.ssh/authorized_keys
 # (So far Deyan is the only one)
 
 my $update_all_cmd = "echo STARTING; hostname; ".
-"cd ~/LaTeXML; git stash; git pull --rebase; git stash pop; cpanm . --notest; ".
-"cd ~/LaTeXML-Plugin-Cortex; git stash; git pull --rebase; git stash pop; cpanm .; ".
+"cd ~/LaTeXML; git stash; git pull --rebase; git stash pop; rm -rf blib/; cpanm --uninstall LaTeXML.pm -f; cpanm . --notest; ".
+"cd ~/LaTeXML-Plugin-Cortex; git stash; git pull --rebase; git stash pop; rm -rf blib/; cpanm --uninstall LaTeXML.pm -f; cpanm .; ".
 "echo ENDING; hostname;";
 
 my $home_update = $update_all_cmd;
@@ -17,7 +17,7 @@ my $hulk_update="ssh deyan\@cortex.mathweb.org \"ssh dginev\@10.70.2.212 \\\"ssh
 # Threads in a perl script? Hah! Let's fork it like it's 1989
 if (my $pid_cortex = fork()) {
   print STDERR `$cortex_update`,"\n\n";
-  waitpid($pid_cortex, 0); 
+  waitpid($pid_cortex, 0);
 } elsif (my $pid_beryl = fork()) {
   print STDERR `$beryl_update`,"\n\n";
   waitpid($pid_beryl, 0);
